ecadc0ac55e2c427aba5cef600427b0fc18235f2
WojciechPtas <wojciech.ptas@gmail.com>
2023-03-16 14:45:56 +0100

    saves to csv

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import math
 from serial import Serial
+from scipy.spatial import distance_matrix
+import numpy as np
 import json
-
-GPS_LIMIT = 50
+import pandas as pd
+GPS_LIMIT = 10
 ENABLE_DEBUG_LOGS = True
 
 class Point:
@@ -33,7 +35,7 @@ def gps_data_to_point(data):
 
     return Point(lat, long)
 
-def get_position(com_port):
+def get_position(com_port:str,user_input:str):
     positions = []
     gps_serial = Serial(com_port)
     
@@ -42,34 +44,43 @@ def get_position(com_port):
             line = str(gps_serial.readline(), encoding="ASCII")
             if "GPGGA" in line:
                 data = line.split(',')
-                if (int(data[7]) > 0): #enough satellites
+                if (int(data[7]) >= 0): #enough satellites
                     print(len(positions))
                     positions.append((gps_data_to_point(data)))
                 else:
                     print("Not enough satelites", data[7])
         except(UnicodeDecodeError):
             print("UnicodeDecodeError!")
+    
     x_list = []
     y_list = []
+    
     for pos in positions:
         x_list.append(pos.x)
         y_list.append(pos.y)
-    x_list.sort()
-    y_list.sort()
     if ENABLE_DEBUG_LOGS:
-        f = open("debug_logs.txt", "w+")
-        for i in range(GPS_LIMIT):
-            f.write(f"#{i} x:{x_list[i]} y:{y_list[i]}")
-        f.close()
+        with open("./data/debug_logs_"+user_input+".txt", "w+") as f:
+            for i in range(GPS_LIMIT):
+                f.write(f"#{i} x:{x_list[i]} y:{y_list[i]}")
+    df = pd.DataFrame(list(zip(x_list, y_list)),
+           columns =['x', 'y'])
+    
+    df.to_csv("./data/"+user_input+".csv")
+    x_list.sort()
+    y_list.sort()   
     return Point(x_list[int(GPS_LIMIT/2)], y_list[int(GPS_LIMIT/2)])
 
-
+def get_medioid_index(df: pd.DataFrame)->int:
+    m = distance_matrix(df.values, df.values)
+    i = np.argmin(m.sum(axis=0))
+    return i
+    
 def read_json_gps_points():
     f = open('GPSdata.json')
     return json.load(f)
 
 if __name__ == "__main__":
-
+    
     user_input = ""
     points = []
 
@@ -79,7 +90,7 @@ if __name__ == "__main__":
         user_input = input("Please input device address, q to quit\n")
         if(user_input == "q"):
             break
-        gps_point = get_position(com_port)
+        gps_point = get_position(com_port,user_input)
         point = {'x': gps_point.x, 'y': gps_point.y, "address": user_input}
         points.append(point)
 
